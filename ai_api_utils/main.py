72b9f6ce6caead7ce258bcb1fadff0e790cbe037
ji-podhead <you@example.com>
2025-03-27 14:17:44 +0100

    added errorhandling for missing api keys

--- ai_api_utils/main.py
+++ ai_api_utils/main.py
@@ -11,10 +11,21 @@ import subprocess
 from pathlib import Path
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
-gemini_client = genai.Client(api_key=GEMINI_API_KEY)
-deepseek_api_key = os.environ["DEEPSEEK_API_KEY"]
-deep_client = OpenAI(api_key=deepseek_api_key, base_url="https://api.deepseek.com")
+try:
+    GEMINI_API_KEY = os.environ["GEMINI_API_KEY"]
+    gemini_client = genai.Client(api_key=GEMINI_API_KEY)
+except KeyError as e:
+ print("no gemini api key found")
+ gemini_client=None
+ GEMINI_API_KEY = None
+try:
+    deepseek_api_key = os.environ["DEEPSEEK_API_KEY"]
+    deep_client = OpenAI(api_key=deepseek_api_key, base_url="https://api.deepseek.com") 
+except KeyError as e:
+ print("no deepseek api key found")
+ deepseek_api_key = None
+ deep_client=None
+
 MAX_TOKENS = int(os.getenv("MAX_TOKENS", "256"))
 gemini_models=gemini_client.models.list()
 
